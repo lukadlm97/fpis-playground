0d1f4c25edc38146cec0c57fa0ecd1f4c5b21eee
Luka Radovanovic <lukadlm97@gmail.com>
2020-03-29 22:31:32 +0200

    useEffects and useState with typescript

--- fpis-playground/src/index.tsx
+++ fpis-playground/src/index.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-const rootElement =  document.getElementById('root')
 // const children = "hello world"
 // const className = "container"
 // const helloElement = React.createElement('span',null,'Hello')
@@ -17,13 +15,32 @@ const rootElement =  document.getElementById('root')
 //   worldElement
 //   )
 
-const element = <React.Fragment>
-  <span>Hello</span> <span>World</span>
-</React.Fragment>
+const  Greeting = () =>{
+  const [name,setName] = useState(window.localStorage.getItem('name'||''))
+
+  React.useEffect(()=>{
+    window.localStorage.setItem('name',name||'')
+  })
+
+  function onChange(e:React.ChangeEvent<HTMLInputElement>){
+    setName(e.target.value)
+  }
+  
+  return <div>
+      <form>
+        <label>Name: </label>
+        <input name="name" onChange={onChange} id="name"/>
+      </form>
+      {name? <strong>Hello {name}</strong>:'Please type your name'}
+    </div>
+}
+
+// const element = <React.Fragment>
+//   <span>Hello</span> <span>World</span>
+// </React.Fragment>
 ReactDOM.render(
-  element,
-  rootElement
- 
+  <Greeting />,
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change

d78373d095f852727098e20e2a7f667f0a1ad2fe
Luka Radovanovic <lukadlm97@gmail.com>
2020-03-31 19:35:50 +0200

    installed required packages for react-route-dom and material-ui

--- fpis-playground/src/App.tsx
+++ fpis-playground/src/App.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Switch,Route, Redirect,Link} from 'react-router-dom'
 
-function App() {
+const App: React.FC =() =>  {
   return (
    <>
+      <div>
+        <Link to="/">Home Page</Link>
+        <span>|</span>
+        <Link to="/about">About Page</Link>
+      </div>
+      <Switch>
+          <Route exact path="/">
+            <h1>Home Page</h1>
+          </Route>
+          <Route exact path="/about">
+            <h1>Aboute Page</h1>
+          </Route>
+          <Redirect to="/"/>
+      </Switch>
    </>
   );
 }
--- fpis-playground/src/index.tsx
+++ fpis-playground/src/index.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router } from 'react-router-dom';
 // const children = "hello world"
 // const className = "container"
 // const helloElement = React.createElement('span',null,'Hello')
@@ -15,36 +16,38 @@ import * as serviceWorker from './serviceWorker';
 //   worldElement
 //   )
 
-function useLocalStorageState(key:any,defalutValue=''){
-  const [state,setState] = React.useState(
-    () => window.localStorage.getItem(key)||defalutValue
-  )
-  React.useEffect(()=>{
-    window.localStorage.setItem(key,state),[state]
-  })
-  return [state,setState]
-}
+// function useLocalStorageState(key:any,defalutValue=''){
+//   const [state,setState] = React.useState(
+//     () => window.localStorage.getItem(key)||defalutValue
+//   )
+//   React.useEffect(()=>{
+//     window.localStorage.setItem(key,state),[state]
+//   })
+//   return [state,setState]
+// }
 
-const  Greeting = () =>{
-  const [name,setName] = useLocalStorageState('name')
+// const  Greeting = () =>{
+//   const [name,setName] = useLocalStorageState('name')
 
-  const onChange = null
+//   const onChange = null
   
-  return <div>
-      <form>
-        <label>Name: </label>
-        <input name="name"  id="name"/>
-      </form>
-      {name? <strong>Hello {name}</strong>:'Please type your name'}
-    </div>
-}
+//   return <div>
+//       <form>
+//         <label>Name: </label>
+//         <input name="name"  id="name"/>
+//       </form>
+//       {name? <strong>Hello {name}</strong>:'Please type your name'}
+//     </div>
+// }
 
 // const element = <React.Fragment>
 //   <span>Hello</span> <span>World</span>
 // </React.Fragment>
 ReactDOM.render(
-  <Greeting />,
-  document.getElementById('root')
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change

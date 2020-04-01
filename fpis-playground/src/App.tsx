924ec68f791f80f24c73bbbe73aec3c99c98abff
Luka Radovanovic <lukadlm97@gmail.com>
2020-04-01 19:11:26 +0200

    fixed bug with appbar

--- fpis-playground/src/App.tsx
+++ fpis-playground/src/App.tsx
@@ -4,12 +4,12 @@ import {Switch,Route, Redirect,Link} from 'react-router-dom'
 
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import useStyles from './style'
 import { useStoreState } from './hooks';
 import Home from '../src/home'
+import Container from '@material-ui/core/Container';
 
 
 const App: React.FC =() =>  {
@@ -24,27 +24,24 @@ const App: React.FC =() =>  {
       <AppBar position="static">
         <Toolbar>
           <IconButton edge="start"  color="inherit" aria-label="menu">
-
+          🍔
           </IconButton>
-          <Button component={Link} color="inherit">Home</Button>
-          <Button component={Link} color="inherit">About</Button>
+            <div>
+            <Button  component={Link} to="/" color="inherit">Home</Button>
+            <Button component={Link} to="/about" color="inherit">About</Button>
+          </div>
         </Toolbar>
       </AppBar>
       <>
-        <div>
-          <Link to="/">Home Page</Link>
-          <span>|</span>
-          <Link to="/about">About Page</Link>
-        </div>
+      <Container maxWidth="md">
         <Switch>
-            <Route exact path="/" component={Home}>
-              <h1>Home Page</h1>
-            </Route>
+            <Route exact path="/" component={Home}/>
             <Route exact path="/about" component={Home}>
               <h1>Aboute Page</h1>
             </Route>
             <Redirect to="/"/>
         </Switch>
+      </Container>
     </>
     </div>
   );

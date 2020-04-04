3b95a0a59888bab480be527d9ab8b96f37ef800c
Luka Radovanovic <lukadlm97@gmail.com>
2020-04-05 00:50:47 +0200

    wotking on form submit

--- fpis-playground/src/GuestBookEntryForm/index.tsx
+++ fpis-playground/src/GuestBookEntryForm/index.tsx
@@ -3,18 +3,21 @@ import TextField from '@material-ui/core/TextField'
 import useStyles from './styles'
 import Button from '@material-ui/core/Button'
 import Box from '@material-ui/core/Box'
+import {useForm} from 'react-hook-form'
+import GuestBookEntry from '../interfaces/GuestBookEntry'
 
 const GuestBookEntryForm = ()=>{
     const classes = useStyles()
-
+    const {register,handleSubmit} = useForm()
+    const onSubmit = (data:GuestBookEntry):void=>{
+        console.log(data)
+    }
     return(
-        <form className={classes.formContainer} noValidate>
-            <TextField label="Name" name="name" variant="outlined" fullWidth/>
-            <TextField multiline rows={3} label="Content" variant="outlined" name="contant" fullWidth/>
+        <form onSubmit={handleSubmit(onSubmit)}  className={classes.formContainer} noValidate>
+            <TextField inputRef={register} label="Name" name="name" variant="outlined" fullWidth/>
+            <TextField inputRef={register} multiline rows={3} label="Content" variant="outlined" name="contant" fullWidth/>
             <Box display="flex" justifyContent="flex-end">
-
-            <Button color="primary" variant="contained" >Add Entry</Button>
-      
+                <Button type="submit" color="primary" variant="contained" >Add Entry</Button>
             </Box>
        </form>
     )

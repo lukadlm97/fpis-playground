dd5af6a58be2c0e9ccaeca35ac9ba98a4b004ed9
Luka Radovanovic <lukadlm97@gmail.com>
2020-04-05 00:02:25 +0200

    created form and installed form-hooks

--- fpis-playground/src/GuestBookEntryForm/index.tsx
+++ fpis-playground/src/GuestBookEntryForm/index.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import TextField from '@material-ui/core/TextField'
 import useStyles from './styles'
+import Button from '@material-ui/core/Button'
+import Box from '@material-ui/core/Box'
 
 const GuestBookEntryForm = ()=>{
     const classes = useStyles()
 
     return(
         <form className={classes.formContainer} noValidate>
-            <TextField label="name" name="name" fullWidth/>
-        </form>
+            <TextField label="Name" name="name" variant="outlined" fullWidth/>
+            <TextField multiline rows={3} label="Content" variant="outlined" name="contant" fullWidth/>
+            <Box display="flex" justifyContent="flex-end">
+
+            <Button color="primary" variant="contained" >Add Entry</Button>
+      
+            </Box>
+       </form>
     )
 };
 
--- fpis-playground/src/GuestBookEntryForm/styles.ts
+++ fpis-playground/src/GuestBookEntryForm/styles.ts
@@ -4,5 +4,8 @@ import {makeStyles} from '@material-ui/core/styles'
 export default makeStyles({
     formContainer:{
         margin:'1rm',
+        '& .MuiTextField-root':{
+            margin:'1rem 0'
+        }
     }
 });
